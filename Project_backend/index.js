e05d4386155124bbf819778ebea7da4a5567ab83
alpharaghav301 <raghav.mittal@alphanodus.com>
2023-05-18 19:52:11 +0530

    some-basic-changes

--- Project_backend/index.js
+++ Project_backend/index.js
@@ -10,15 +10,15 @@ mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser:true,  useUnifiedTopology: true },
 );
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
+const userRoute = require("./routers/user");
+// const authRoute = require("./routers/auth");
+// const postRoute = require("./routers/posts");
   app.use(express.json());
   app.use(helmet());
   app.use(morgan("common"));
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
+// app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+// app.use("/api/posts", postRoute);
  
   app.listen(8800,()=>{
     console.log("backend server is");
